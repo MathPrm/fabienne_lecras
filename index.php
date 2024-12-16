31f3b86db674b412c4ad738a1c33683d9553b55c
MathPrm <primotmathilde@gmail.com>
2024-12-16 11:29:33 +0100

    theme created

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fabienne Lecras Tanguy</title>
+</head>
+<body>
+    <h1>Toto</h1>
+</body>
+</html>
