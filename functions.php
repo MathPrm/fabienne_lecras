f120207385a62aaa48c1f3e03a8e2bc02a2b0754
Chaimaa ZEROUAL <chaimaa1250zeroual@gmail.com>
2024-12-29 13:28:10 +0100

    page 1

--- functions.php
+++ functions.php
@@ -1,10 +1,7 @@
  <?php
 
   function theme_enqueue_styles() {
-    // Lier la feuille de style principale
     wp_enqueue_style( 'main-style', get_stylesheet_uri() );
-
-    // Si vous avez un fichier CSS personnalisé :
     wp_enqueue_style( 'custom-style', get_template_directory_uri() . '/style.css' );
 }
 
