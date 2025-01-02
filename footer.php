b03e1a9a851e82a74037a9fbf9914907232d23b5
MathPrm <primotmathilde@gmail.com>
2025-01-03 00:14:23 +0100

    feat(footer): add footer and footer style

--- footer.php
+++ footer.php
@@ -1,3 +1,14 @@
+    <footer>
+        <div>
+            <?php dynamic_sidebar( 'bas-gauche' );?>
+        </div>
+        <div>
+            <?php dynamic_sidebar( 'bas-centre' );?>
+        </div>
+        <div>
+            <?php dynamic_sidebar( 'bas-droite' );?>
+        </div>
+    </footer>
     <?php wp_footer() ?>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -3,7 +3,11 @@
 function montheme_supports () {
     add_theme_support('title-tag');
     add_theme_support('menus');
-    register_nav_menu('header', 'En tête du menu');
+    register_nav_menus(array(
+        'header' => 'En tête du menu',
+        'footer_left' => 'Pied de page gauche',
+        'footer_right' => 'Pied de page droite',
+    ));
     add_theme_support('custom-logo', array(
         'height' => 100,
         'width' => 400,
@@ -28,3 +32,22 @@ function montheme_title_separator() {
     return '|';
 }
 add_filter('document_title_separator', 'montheme_title_separator');
+
+function mon_theme_widget_zone(){
+	register_sidebar([
+		'name' => __('bas_gauche', 'monTheme'),
+		'id' => 'bas-gauche',
+		'description' => 'region en bas à gauche pour insérer un widget'
+	]);
+	register_sidebar([
+		'name' => __('bas_centre', 'monTheme'),
+		'id' => 'bas-centre',
+		'description' => 'region en bas au centre pour insérer un widget'
+	]);
+	register_sidebar([
+		'name' => __('bas_droite', 'monTheme'),
+		'id' => 'bas-droite',
+		'description' => 'region en bas à droite pour insérer un widget'
+	]);
+}
+add_action('widgets_init', 'mon_theme_widget_zone');
