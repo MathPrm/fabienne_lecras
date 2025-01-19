df80348e209f5b4b289c5379d55c8c0f3715a80e
Chaimaa ZEROUAL <chaimaa1250zeroual@gmail.com>
2025-01-19 18:02:29 +0100

    suupression-de-titre

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<!-- The WordPress Loop -->
 	<p>Output the post data. The functions for the loop work here, e.g., the_title() </p>
-	<h2><?php the_title() ?></h2>
 <?php endwhile; else : ?>
 	<p>No posts.</p>
 <?php endif; ?>
