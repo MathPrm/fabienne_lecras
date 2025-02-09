bbb2ee5a510d5697e2ecdbe5b71c41c05d04ae17
MathPrm <primotmathilde@gmail.com>
2025-02-09 13:27:01 +0100

    add footer file

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
     <footer>
         <div id="menu-2">
-            <?php dynamic_sidebar( 'bas-gauche' );?>
+            <?php dynamic_sidebar( 'bas-gauche' ); ?>
         </div>
         <div id="footer-logo">
-            <?php dynamic_sidebar( 'bas-centre' );?>
+            <?php dynamic_sidebar( 'bas-centre' ); ?>
         </div>
         <div id="menu-3">
-            <?php dynamic_sidebar( 'bas-droite' );?>
+            <?php dynamic_sidebar( 'bas-droite' ); ?>
         </div>
         <div id="reseaux">
-            <?php dynamic_sidebar( 'reseaux' );?>
+            <?php dynamic_sidebar( 'reseaux' ); ?>
         </div>
     </footer>
     <?php wp_footer(); ?>
