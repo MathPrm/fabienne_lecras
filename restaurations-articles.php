2fc0ff91f87aa67b88c750bc36c3f8021d7afefb
MathPrm <primotmathilde@gmail.com>
2025-02-09 13:37:46 +0100

    add articles

--- restaurations-articles.php
+++ restaurations-articles.php
@@ -16,7 +16,7 @@ get_header();
         <div class="restaurations-submenu">
             <div class="restaurations-submenu-container">
                 <div class="restaurations-menu-logo-container-1">
-                    <?php $logo_field = get_field('image_1') ?>
+                    <?php $logo_field = get_field('image_1'); ?>
                     <?php if ($logo_field) : ?>
                         <img src="<?php the_field('image_1'); ?>">
                     <?php endif; ?>
